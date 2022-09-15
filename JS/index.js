2e3572d7f52ca4f1afad1e2d46f45d464e5decf2
Cristian Origuen <107323524+OriguenCristian@users.noreply.github.com>
2022-09-15 18:03:57 -0500

    .

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,15 @@
 let palabras = ["ALURA","ORACLE","ONE","HTML","MADRE","ALEGRIA","PULGA"];
-let tablero = document.getElementById("canvas").getContext("2d");
+//let tablero = document.getElementById("canvas").getContext("2d");
 let palabraSecreta = "";
 
-//iniciar Juego conel boton
-function iniciarJuego(){
-    document.getElementById("inicioJuego").style.display = "none";
-
-    palabraSecretaAleatoria();
-    canvasDibujo();
-}
-
+//eEnerar palabra aleatoria
 function palabraSecretaAleatoria(){
     let palabra = palabras[Math.floor(Math.random() * palabras.length)];
     palabraSecreta = palabra;
-    console.log(palabraSecreta)
+    console.log(palabraSecreta);
 }
+//iniciar Juego conel boton INICIAR JUEGO
+function iniciarJuego(){
+    palabraSecretaAleatoria();
+}
+iniciarJuego()
