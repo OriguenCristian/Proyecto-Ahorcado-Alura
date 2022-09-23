bb8b97f1246f0d823ff9d3c554a9cdfda6b57615
Cristian Origuen <107323524+OriguenCristian@users.noreply.github.com>
2022-09-23 09:14:30 -0500

    Cristian Origuen

--- JS/index.js
+++ JS/index.js
@@ -1,23 +1,33 @@
 const palabras=["ALURA","ORACLE","ONE","HTML","MADRE","ALEGRIA","PULGA"];
+const btnTeclado = document.querySelectorAll("#teclado .tecla");
+const btnInicioJuego =document.getElementById("btnInioJuego");
+const btnNuevoJuego = document.getElementById("btnNuevoJuego");
+//btnInicioJuego.addEventListener("click", inicio)
+btnNuevoJuego.addEventListener("click", iniciarJuego)
+
 let palabraSecreta = "";
 let vidas = 8;
 let acertadas = 0;
 
+ function inicio(){};
+   
 function id(str){
     return document.getElementById(str);    
 }
 
 //iniciar Juego conel boton INICIAR JUE
 function iniciarJuego(){
+    btnNuevoJuego.disabled=true;
+    tecladoP()
     palabraSecretaAleatoria();
-    vidas = 8;
+    vidas = 0;
     acertadas = 0;
-
-    
-
-    console.log(palabraSecreta)
+    console.log(palabraSecreta);
+    const btnTeclado = document.querySelectorAll("#teclado .tecla");
+    for (let i = 0; i < btnTeclado.length; i++){
+        btnTeclado[i].disabled = false;
+     } 
 }
-
 //geEnerar palabra aleatoria e introduce as lineas de la cantidad de letras de la palabra.
 function palabraSecretaAleatoria(){
     
@@ -35,26 +45,47 @@ function palabraSecretaAleatoria(){
     }
 }
 
-//funcion Para el teclado y comparacion de las teclas presionadas con las palabras 
-
-const btnTeclado = document.querySelectorAll("#teclado .tecla");
-    for (let i = 0; i < btnTeclado.length; i++){
-        btnTeclado[i].addEventListener( "click", clickTeclado);
+//funcion Para el teclado y comparacion de las teclas presionadas con las palabras.
+function tecladoP(){
+ for (let i = 0; i < btnTeclado.length; i++){
+    btnTeclado[i].addEventListener( "click", clickTeclado);
     }
+ }
 
 function clickTeclado(event){
-    let palabraPresionada= document.querySelectorAll("#palabraSecreta span");
+    let palabraSelec= document.querySelectorAll("#palabraSecreta span");
     const button = event.target;
     button.disabled = true;
-    const letraPresionada = button.innerHTML.toLocaleUpperCase();
-    const palabraSecrMayus = palabraSecreta.toLocaleUpperCase();
+    const letraPresionada = button.innerHTML.toUpperCase();
+    const palabraSecrMayus = palabraSecreta.toUpperCase();
    
     let  acerto = false;
     for(let i = 0; i < palabraSecrMayus.length; i++){
         if ( letraPresionada == palabraSecrMayus[i]){
-            palabraPresionada[i].innerHTML =letraPresionada;
+            palabraSelec[i].innerHTML =letraPresionada;
             acertadas++;
+            console.log(acertadas)
             acerto = true;
         }
     }
+    if (acerto == false){
+        vidas++;
+    }
+    if (vidas == 7){
+        alert("perdiste la palabra era" + palabraSecreta);
+        finJuego()
+    }
+     else if (acertadas == palabraSecreta.length){
+    alert("felicidades ganaste");
+    finJuego()
+    
+}
+}
+function  finJuego(){
+    const btnTeclado = document.querySelectorAll("#teclado .tecla");
+    for (let i = 0; i < btnTeclado.length; i++){
+        btnTeclado[i].disabled = true;
+     } 
+     btnNuevoJuego.disabled= false;
+     
 }
