9fe23c2a1e115014026a4b3c09d2f41aad93f553
Cristian Origuen <cm.origuen@gmail.com>
2024-06-28 17:30:32 -0400

    Actualizacion

--- JS/Palabring.js
+++
@@ -1,8 +0,0 @@
-const inputPalabraIngresada = document.getElementById("textIngresado")
-export let palabraIngresada = []
- 
-const getValueInput =() => {
-  
-  palabraIngresada.push(inputPalabraIngresada.value)
-  console.log(palabraIngresada)
-}
